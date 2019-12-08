51bf23376c3644329d698472895ac057ddc0898c
wangshengyan <shengyan.wang@51zouchuqu.com>
2019-12-08 09:25:09 +0800

    添加代码

--- app/src/main/java/com/example/dawn/myapplication/MyApplication.java
+++ app/src/main/java/com/example/dawn/myapplication/MyApplication.java
@@ -1,6 +1,7 @@
 package com.example.dawn.myapplication;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.meituan.android.walle.WalleChannelReader;
 import com.umeng.commonsdk.UMConfigure;
@@ -16,6 +17,7 @@ public class MyApplication extends Application{
     private void initUMengChannel() {
         //  获取walle的渠道号
         String channel = WalleChannelReader.getChannel(this.getApplicationContext());
+        Log.d("initUMengChannel", "initUMengChannel : "+channel);
         // 获取渠道号以后设置到友盟中
         UMConfigure.init(this, "5386fcc256240be6cf00c687", channel, UMConfigure.DEVICE_TYPE_PHONE, "1fe6a20054bcef865eeb0991ee84525b");
 
